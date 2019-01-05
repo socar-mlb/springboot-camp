b92ced985e72643e54a85886dba3ae8f082cf157
socar-mlb <mlb@socar.kr>
2019-01-05 14:56:37 +0900

    spring project initialize by SPRING INITIALIZR

---
+++ spring01/src/test/java/org/inframincer/spring01/Spring01ApplicationTests.java
@@ -0,0 +1,17 @@
+package org.inframincer.spring01;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring01ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
