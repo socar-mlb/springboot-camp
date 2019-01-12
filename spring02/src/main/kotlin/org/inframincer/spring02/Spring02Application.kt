f09faa6f9d5994108c9b93c73d8bfd48629dc44d
socar-mlb <mlb@socar.kr>
2019-01-12 14:25:48 +0900

    spring02 initializr (web, devtools, lombok, thymeleaf)

---
+++ spring02/src/main/kotlin/org/inframincer/spring02/Spring02Application.kt
@@ -0,0 +1,12 @@
+package org.inframincer.spring02
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Spring02Application
+
+fun main(args: Array<String>) {
+	runApplication<Spring02Application>(*args)
+}
+
---
+++ spring02/src/test/kotlin/org/inframincer/spring02/Spring02ApplicationTests.kt
@@ -0,0 +1,17 @@
+package org.inframincer.spring02
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class Spring02ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
+
