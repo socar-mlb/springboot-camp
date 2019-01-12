ce6a2621ae22356d9f5e7fe31550873994dfe7c8
socar-mlb <mlb@socar.kr>
2019-01-12 16:32:19 +0900

    custom configuration by WebMvcConfigurer

---
+++ spring02/src/main/kotlin/org/inframincer/spring02/config/WebConfigurer.kt
@@ -0,0 +1,9 @@
+package org.inframincer.spring02.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+
+@Configuration
+//class WebConfigurer : WebMvcConfigurerAdapter // This type has a constructor, and thus must be initialized here
+class WebConfigurer : WebMvcConfigurer
