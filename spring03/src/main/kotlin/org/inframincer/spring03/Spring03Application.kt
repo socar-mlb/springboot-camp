c51733a9271bb73a24daa4c5fd177990c41a778d
socar-mlb <mlb@socar.kr>
2019-01-19 16:07:37 +0900

    spring jpa initializr

---
+++ spring03/src/main/kotlin/org/inframincer/spring03/Spring03Application.kt
@@ -0,0 +1,12 @@
+package org.inframincer.spring03
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Spring03Application
+
+fun main(args: Array<String>) {
+    runApplication<Spring03Application>(*args)
+}
+
---
+++ spring03/src/test/kotlin/org/inframincer/spring03/Spring03ApplicationTests.kt
@@ -0,0 +1,17 @@
+package org.inframincer.spring03
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class Spring03ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
+
