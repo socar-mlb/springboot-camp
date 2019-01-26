59cc9b663ba8c4c15f48f0926dffb6f472f12759
socar-mlb <mlb@socar.kr>
2019-01-26 15:59:15 +0900

    spring jpa, security initializr

---
+++ spring04/src/main/kotlin/org/inframincer/spring04/Spring04Application.kt
@@ -0,0 +1,12 @@
+package org.inframincer.spring04
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Spring04Application
+
+fun main(args: Array<String>) {
+	runApplication<Spring04Application>(*args)
+}
+
